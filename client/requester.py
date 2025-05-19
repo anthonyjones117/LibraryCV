10a5cd9bec2f80254ceff7bdb7f253152c1a7fb2
anthonyjones117 <anthonyjones3281@gmail.com>
2025-05-19 17:17:36 -0400

    setup server and client

---
+++ client/requester.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import requests
+res = requests.post('http://127.0.0.1:5000/data', json={"mytext":"lalala"})
+if res.ok:
+    print(res.json())
--- data_insert.py
+++ data_insert.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
-import pymongo
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+app = Flask(__name__)
 
-mydict = { "name": "John", "address": "Highway 37" }
+# Configure MongoDB connection
+MONGO_URI = "mongodb://localhost:27017/"  # Replace with your MongoDB URI
+DATABASE_NAME = "mydatabase"
+COLLECTION_NAME = "customers"
 
-x = mycol.insert_one(mydict)
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+mycol = db[COLLECTION_NAME]
+
+#mydict = { "name": "John", "address": "Highway 37" }
+
+#x = mycol.insert_one(mydict)
+
+@app.route('/data', methods=['POST'])
+def add_data():
+    data = request.get_json()
+    if data:
+        result = mycol.insert_one(data)
+        return jsonify({'message': 'Data added successfully', 'inserted_id': str(result.inserted_id)}), 201
+    else:
+        return jsonify({'message': 'No data provided'}), 400
+    
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True)
